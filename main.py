d1b9d271c2a8ab8a45c54945183ec39ad6cc08a8
Vladimir <bigcorvet@gmail.com>
2023-01-25 17:45:07 +0200

    added main function

--- main.py
+++ main.py
@@ -17,9 +17,19 @@ def download_image(url=''):
 def download_video(url=''):
     try:
         r = requests.get(url)
-        with open('r_video', 'wb') as f:
-            f.write(r.content)
+        with open('r_video.mp4', 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1200 * 768):
+                f.write(chunk)
+
         return 'Видео успешно загружено!'
     
     except Exception as _ex:
         return 'Что-то пошло не так! Проверьте url' 
+
+
+def main():
+    print(download_image(url=img_url))
+    print(download_video(url=video_url))
+
+if __name__=='__main__':
+    main()
